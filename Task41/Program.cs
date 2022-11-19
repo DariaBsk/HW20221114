5b4c60fa5195bfb7573380d049366840d2284f6c
DariaBsk <Dariabsk602@gmail.com>
2022-11-19 18:35:43 +0300

    Добавили решение задач

---
+++ Task41/Program.cs
@@ -0,0 +1,58 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел.
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+// 0, 7, 8, -2, -2 -> 2
+
+// -1, -7, 567, 89, 223-> 3
+
+
+int[] CreateArrayUserInt(int size)
+{
+    int[] array = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        Console.WriteLine("Введите элемент массива:");
+        array[i] = Convert.ToInt32(Console.ReadLine());
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i]);
+        if (i < array.Length - 1) Console.Write(", ");
+    }
+    Console.WriteLine("]");
+}
+
+
+int PositiveNums(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0) count++;
+    }
+    return count;
+}
+
+
+Console.Write("Введите количество элементов массива: ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+if (m <= 0)
+{
+    Console.WriteLine($"Число {m} некорректно, нужно ввести целое положительное число.");
+}
+else
+{
+    int[] createArrayUserInt = CreateArrayUserInt(m);
+    PrintArray(createArrayUserInt);
+    int positiveNums = PositiveNums(createArrayUserInt);
+    
+    Console.WriteLine($"Количество положительных чисел в массиве: {positiveNums}");
+
+}
---
+++ Task43/Program.cs
@@ -0,0 +1,49 @@
+﻿// Задача 43: Напишите программу, которая найдёт
+// точку пересечения двух прямых,
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2;
+// значения b1, k1, b2 и k2 задаются пользователем.
+
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+double CoordinatesX(double k01, double k02, double b01, double b02)
+{
+    double coordinatesX = (b02-b01)/(k01-k02);
+    return coordinatesX;
+}
+
+double CoordinatesY(double coordinatesX, double k01, double b01)
+{
+    double coordinatesY = k01*coordinatesX+b01;
+    return coordinatesY;
+}
+
+
+Console.WriteLine("Введите данные прямых m и n. ");
+
+Console.Write("k1 = ");
+double k1 = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("k2 = ");
+double k2 = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("b1 = ");
+double b1 = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("b2 = ");
+double b2 = Convert.ToDouble(Console.ReadLine());
+
+if (k1 == k2 && b1 == b2)
+{
+    Console.WriteLine("Прямые совпадают.");
+}
+else if(k1 == k2 && b1 != b2)
+{
+    Console.WriteLine("Прямые не пересекаются, они параллельны.");
+}
+else
+{
+    double coordinsX = CoordinatesX(k1, k2, b1, b2);
+    double coordinsY = CoordinatesY(coordinsX, k1, b1);
+
+    Console.WriteLine($"Координаты точки пересечения прямых m и n: ({coordinsX}; {coordinsY})");
+}
